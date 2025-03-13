2de5b5db862af4641802c34ac2377e2ee8fc64bd
alizzelol <alizzelol@gmail.com>
2025-03-13 11:58:22 +0100

    xml login2

--- app/src/main/java/com/alizzelol/adaechat/LoginActivity.java
+++ app/src/main/java/com/alizzelol/adaechat/LoginActivity.java
@@ -2,15 +2,10 @@ package com.alizzelol.adaechat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -18,65 +13,60 @@ import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText etEmail, etContraseña;
-    private Button btnLogin;
-    private FirebaseAuth mAuth;
-    private FirebaseFirestore db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etEmail = findViewById(R.id.etEmail);
-        etContraseña = findViewById(R.id.etContraseña);
-        btnLogin = findViewById(R.id.btnLogin);
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
+        EditText etEmail = findViewById(R.id.etEmail);
+        EditText etContrasena = findViewById(R.id.etContrasena);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = etEmail.getText().toString();
-                String contraseña = etContraseña.getText().toString();
+        btnLogin.setOnClickListener(v -> iniciarSesionUsuario(etEmail, etContrasena, mAuth, db));
+    }
 
-                mAuth.signInWithEmailAndPassword(email, contraseña)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Obtener el nombre de usuario del correo electrónico (asumiendo que el nombre de usuario es la parte antes de "@")
-                                    String usuario = email.substring(0, email.indexOf("@"));
+    private void iniciarSesionUsuario(EditText etEmail, EditText etContrasena, FirebaseAuth mAuth, FirebaseFirestore db) {
+        String email = etEmail.getText().toString();
+        String contrasena = etContrasena.getText().toString();
 
-                                    db.collection("users").document(usuario).get()
-                                            .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                    if (task.isSuccessful() && task.getResult() != null) {
-                                                        DocumentSnapshot document = task.getResult();
-                                                        if (document.exists()) {
-                                                            Map<String, Object> userData = document.getData();
-                                                            String username = (String) userData.get("username");
+        mAuth.signInWithEmailAndPassword(email, contrasena)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        String usuario = email.substring(0, email.indexOf("@"));
 
-                                                            Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show();
-                                                            Intent intent = new Intent(LoginActivity.this, ChatActivity.class);
-                                                            intent.putExtra("username", username);
-                                                            startActivity(intent);
-                                                            finish();
-                                                        } else {
-                                                            Toast.makeText(LoginActivity.this, "Error al obtener los datos del usuario.", Toast.LENGTH_SHORT).show();
-                                                        }
-                                                    } else {
-                                                        Toast.makeText(LoginActivity.this, "Error al obtener los datos del usuario.", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                }
-                                            });
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "Error en el inicio de sesión: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
+                        db.collection("users").document(usuario).get()
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful() && task1.getResult() != null) {
+                                        DocumentSnapshot document = task1.getResult();
+                                        if (document.exists()) {
+                                            Map<String, Object> userData = document.getData();
+                                            if (userData != null) { // Agregamos esta verificación
+                                                String username = (String) userData.get("username");
+
+                                                Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(LoginActivity.this, ChatActivity.class);
+                                                intent.putExtra("username", username);
+                                                startActivity(intent);
+                                                finish();
+                                            } else {
+                                                Toast.makeText(LoginActivity.this, "Error: Datos del usuario vacíos.", Toast.LENGTH_SHORT).show();
+                                            }
+                                        } else {
+                                            Toast.makeText(LoginActivity.this, "Error al obtener los datos del usuario.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Error al obtener los datos del usuario.", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        if (task.getException() != null) {
+                            Toast.makeText(LoginActivity.this, "Error en el inicio de sesión: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Error en el inicio de sesión.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }

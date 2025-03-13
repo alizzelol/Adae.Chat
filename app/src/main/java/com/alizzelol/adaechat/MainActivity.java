8da20e9a97b9c7387495a230d80412d1c6df7132
alizzelol <alizzelol@gmail.com>
2025-03-13 11:05:13 +0100

    MainActivity

--- app/src/main/java/com/alizzelol/adaechat/MainActivity.java
+++ app/src/main/java/com/alizzelol/adaechat/MainActivity.java
@@ -3,6 +3,7 @@ package com.alizzelol.adaechat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {

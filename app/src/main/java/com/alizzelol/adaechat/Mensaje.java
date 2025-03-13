793d77f5867b91dd9256566d00656eeb18703971
alizzelol <alizzelol@gmail.com>
2025-03-13 13:06:06 +0100

    ConversationActivity

---
+++ app/src/main/java/com/alizzelol/adaechat/Mensaje.java
@@ -0,0 +1,44 @@
+package com.alizzelol.adaechat;
+
+import java.util.Date;
+
+public class Mensaje {
+    private String emisor;
+    private String contenidoMensaje;
+    private Date timestamp;
+
+    public Mensaje() {
+        // Constructor vacío requerido por Firestore
+    }
+
+    public Mensaje(String emisor, String contenidoMensaje, Date timestamp) {
+        this.emisor = emisor;
+        this.contenidoMensaje = contenidoMensaje;
+        this.timestamp = timestamp;
+    }
+
+    // Getters y setters
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public void setEmisor(String emisor) {
+        this.emisor = emisor;
+    }
+
+    public String getContenidoMensaje() {
+        return contenidoMensaje;
+    }
+
+    public void setContenidoMensaje(String contenidoMensaje) {
+        this.contenidoMensaje = contenidoMensaje;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
